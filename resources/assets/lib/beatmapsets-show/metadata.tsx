371b9d60120876266920b84896e7f4a53429f33b
Gagah Pangeran Rosfatiputra <git@gagahpangeran.com>
2021-07-02 14:27:55 +0700

    initial beatmapset stats
    add beatmapset metadata box
    
    add spacer
    
    add ranked date
    
    add beatmap stats
    
    add bar stats
    
    add circle and slider count
    
    add metadata editor
    
    extract renderStat function
    
    fix potential duplicate key tag
    
    change grid size and spacing
    
    simplify edit button opacity
    
    move fancy scrollbar class
    
    increase tag list height
    
    use getter in stats
    
    Remove redundant info
    
    Fix typo
    
    Format all values
    
    Add spacer
    
    Less moving numbers around
    
    Format numbers

---
+++ resources/assets/lib/beatmapsets-show/metadata.tsx
@@ -0,0 +1,151 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
+// See the LICENCE file in the repository root for full licence text.
+
+import BeatmapsetExtendedJson from 'interfaces/beatmapset-extended-json';
+import { route } from 'laroute';
+import { Modal } from 'modal';
+import * as moment from 'moment';
+import * as React from 'react';
+import TimeWithTooltip from 'time-with-tooltip';
+import { UserLink } from 'user-link';
+import MetadataEditor from './metadata-editor';
+
+interface Props {
+  beatmapset: BeatmapsetExtendedJson;
+}
+
+interface State {
+  isEditing: boolean;
+}
+
+export default class Metadata extends React.PureComponent<Props, State> {
+  constructor(props: Props) {
+    super(props);
+
+    this.state = {
+      isEditing: false,
+    };
+  }
+
+  render() {
+    const tags = this.props.beatmapset.tags.split(' ');
+    const canEdit = this.props.beatmapset.current_user_attributes?.can_edit_metadata ?? false;
+
+    return (
+      <div className='beatmapset-metadata u-fancy-scrollbar'>
+        {this.state.isEditing && (
+          <Modal onClose={this.toggleEditing} visible>
+            <MetadataEditor beatmapset={this.props.beatmapset} onClose={this.toggleEditing} />
+          </Modal>
+        )}
+
+        {this.props.beatmapset.source !== '' && (
+          <>
+            <div>
+              {osu.trans('beatmapsets.show.info.source')}
+            </div>
+            <div >
+              <a href={route('beatmapsets.index', { q: this.props.beatmapset.source })}>
+                {this.props.beatmapset.source}
+              </a>
+            </div>
+          </>
+        )}
+
+        <div>
+          {osu.trans('beatmapsets.show.info.genre')}
+        </div>
+        <div >
+          <a href={route('beatmapsets.index', { g: this.props.beatmapset.genre.id })}>
+            {this.props.beatmapset.genre.name}
+          </a>
+        </div>
+
+        <div className='beatmapset-metadata__spacer' />
+
+        <div>
+          {osu.trans('beatmapsets.show.info.language')}
+        </div>
+        <div>
+          <a href={route('beatmapsets.index', { l: this.props.beatmapset.language.id })}>
+            {this.props.beatmapset.language.name}
+          </a>
+        </div>
+
+        {tags.length > 0 && (
+          <>
+            <div>
+              {osu.trans('beatmapsets.show.info.tags')}
+            </div>
+            <div className='beatmapset-metadata__value beatmapset-metadata__value--tags'>
+              {tags.map((tag, idx) => (
+                <React.Fragment key={`${tag}-${idx}`}>
+                  <a href={route('beatmapsets.index', { q: tag })}>
+                    {tag}
+                  </a>
+                  <span> </span>
+                </React.Fragment>
+              ))}
+            </div>
+          </>
+        )}
+
+        <div className='beatmapset-metadata__spacer' />
+
+        <div>
+          {osu.trans('beatmapsets.show.info.submitted')}
+        </div>
+        <div >
+          {this.renderDate(this.props.beatmapset.submitted_date)}
+        </div>
+
+        {this.props.beatmapset.ranked > 0 ? (
+          <>
+            <div>
+              {osu.trans(`beatmapsets.show.info.${this.props.beatmapset.status}`)}
+            </div>
+            <div>
+              {this.renderDate(this.props.beatmapset.ranked_date)}
+            </div>
+          </>
+        ) : (
+          <>
+            <div>
+              {osu.trans('beatmapsets.show.info.updated')}
+            </div>
+            <div>
+              {this.renderDate(this.props.beatmapset.last_updated)}
+            </div>
+          </>
+        )}
+
+        {canEdit && (
+          <div className='beatmapset-metadata__edit-button'>
+            <button
+              className='btn-circle'
+              onClick={this.toggleEditing}
+              type='button'
+            >
+              <span className='btn-circle__content'>
+                <i className='fas fa-pencil-alt' />
+              </span>
+            </button>
+          </div>
+        )}
+      </div>
+    );
+  }
+
+  renderDate(dateTime: string) {
+    return (
+      <TimeWithTooltip
+        dateTime={dateTime}
+        relative={Math.abs(moment().diff(moment(dateTime), 'weeks')) < 4}
+      />
+    );
+  }
+
+  private toggleEditing = () => {
+    this.setState({ isEditing: !this.state.isEditing });
+  };
+}
