a4eb33f477169c4b7c6997c3010a6d4375563f91
Gagah Pangeran Rosfatiputra <git@gagahpangeran.com>
2021-07-02 23:11:13 +0700

    add beatmapset toolbar
    add download buttons
    
    add favourite button
    
    centering toolbar buttons
    
    add discussion button
    
    add login button
    
    use button pink modifier
    
    extract render fav button function
    
    remove key props
    
    mobile layout for toolbar
    
    simplify render favourite button
    
    use utils url
    
    adjust favourite button
    
    adjust toolbar layout
    
    add back report menu
    
    Remove unnecessary key
    
    Use plain string
    
    Remove unused extra class
    
    Remove unused variable
    
    Correct label font weight
    
    Remove redundant link to modding page
    
    Adjust toolbar styling
    
    Mainly removing the thick border (incorrect design).

---
+++ resources/assets/lib/beatmapsets-show/toolbar.tsx
@@ -0,0 +1,167 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
+// See the LICENCE file in the repository root for full licence text.
+
+import { BigButton } from 'big-button';
+import BeatmapJsonExtended from 'interfaces/beatmap-json-extended';
+import BeatmapsetExtendedJson from 'interfaces/beatmapset-extended-json';
+import { route } from 'laroute';
+import core from 'osu-core-singleton';
+import * as React from 'react';
+import { createClickCallback } from 'utils/html';
+import { beatmapDownloadDirect } from 'utils/url';
+import BeatmapsetMenu from './beatmapset-menu';
+
+interface Props {
+  beatmapset: BeatmapsetExtendedJson;
+  currentBeatmap: BeatmapJsonExtended;
+  favcount: number;
+  hasFavourited: boolean;
+}
+
+interface DownloadButtonProps {
+  bottomTextKey?: string;
+  href: string;
+  icon?: string;
+  osuDirect?: boolean;
+  topTextKey?: string;
+}
+
+const DownloadButton = ({
+  bottomTextKey,
+  href,
+  topTextKey = '_',
+}: DownloadButtonProps) => (
+  <BigButton
+    href={href}
+    modifiers='beatmapset-toolbar'
+    props={{
+      'data-turbolinks': false,
+      href,
+    }}
+    text={{
+      bottom: bottomTextKey && osu.trans(`beatmapsets.show.details.download.${bottomTextKey}`),
+      top: osu.trans(`beatmapsets.show.details.download.${topTextKey}`),
+    }}
+  />
+);
+
+export default class Toolbar extends React.PureComponent<Props> {
+  render() {
+    return (
+      <div className='beatmapset-toolbar'>
+        <div className='beatmapset-toolbar__count'>
+          <div>
+            <div>
+              {osu.trans('beatmapsets.show.details.count.total_play')}
+            </div>
+            <div className='beatmapset-toolbar__count-value'>
+              {osu.formatNumber(this.props.beatmapset.play_count)}
+            </div>
+          </div>
+
+          <div>
+            <div>
+              {osu.trans('beatmapsets.show.details.count.diff_play')}
+            </div>
+            <div className='beatmapset-toolbar__count-value'>
+              {osu.formatNumber(this.props.currentBeatmap.playcount)}
+            </div>
+          </div>
+        </div>
+
+        <div className='beatmapset-toolbar__buttons'>
+          {this.renderFavouriteButton()}
+          {this.renderDownloadButtons()}
+          {this.renderLoginButton()}
+          {this.renderMenuButton()}
+        </div>
+      </div>
+    );
+  }
+
+  private renderDownloadButtons() {
+    if (core.currentUser != null && !this.props.beatmapset.availability?.download_disabled) {
+      return (
+        <>
+          {this.props.beatmapset.video ? (
+            <>
+              <DownloadButton
+                bottomTextKey='video'
+                href={route('beatmapsets.download', { beatmapset: this.props.beatmapset.id })}
+              />
+              <DownloadButton
+                bottomTextKey='no-video'
+                href={route('beatmapsets.download', { beatmapset: this.props.beatmapset.id, noVideo: 1 })}
+              />
+            </>
+          ) : (
+            <DownloadButton
+              href={route('beatmapsets.download', { beatmapset: this.props.beatmapset.id })}
+            />
+          )}
+
+          <DownloadButton
+            href={core.currentUser.is_supporter
+              ? beatmapDownloadDirect(this.props.currentBeatmap.id)
+              : route('support-the-game')
+            }
+            osuDirect
+            topTextKey='direct'
+          />
+        </>
+      );
+    }
+  }
+
+  private renderFavouriteButton() {
+    const action = this.props.hasFavourited ? 'unfavourite' : 'favourite';
+    const icon = `${this.props.hasFavourited ? 'fas' : 'far'} fa-heart`;
+
+    return (
+      <button
+        className='btn-osu-big btn-osu-big--beatmapset-favourite btn-osu-big--pink'
+        onClick={this.toggleFavourite}
+        title={osu.trans(`beatmapsets.show.details.${action}`)}
+      >
+        <i className={icon} />
+        {' '}
+        {osu.formatNumber(this.props.favcount)}
+      </button>
+    );
+  }
+
+  renderLoginButton() {
+    if (!currentUser.id) {
+      return (
+        <BigButton
+          extraClasses={['js-user-link']}
+          modifiers='beatmapset-toolbar'
+          text={{
+            bottom: osu.trans('beatmapsets.show.details.login_required.bottom'),
+            top: osu.trans('beatmapsets.show.details.login_required.top'),
+          }}
+        />
+      );
+    }
+  }
+
+  private renderMenuButton() {
+    if (currentUser.id && currentUser.id !== this.props.beatmapset.user_id) {
+      return (
+        <div className='beatmapset-toolbar__menu'>
+          <div className='btn-circle btn-circle--page-toggle'>
+            <BeatmapsetMenu beatmapset={this.props.beatmapset} />
+          </div>
+        </div>
+      );
+    }
+  }
+
+  private toggleFavourite = (e: React.MouseEvent<HTMLElement>) => {
+    if (core.userLogin.showIfGuest(createClickCallback(e.target))) {
+      return;
+    }
+
+    $.publish('beatmapset:favourite:toggle');
+  };
+}
