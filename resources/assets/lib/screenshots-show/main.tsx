7e47947b0af4470595a828d748fbbddb51bdb7dd
Gagah Pangeran Rosfatiputra <git@gagahpangeran.com>
2021-07-24 23:45:52 +0700

    add header and content screenshot

--- resources/assets/lib/screenshots-show/main.tsx
+++ resources/assets/lib/screenshots-show/main.tsx
@@ -4,6 +4,8 @@
 import HeaderV4 from 'header-v4';
 import ScreenshotJson from 'interfaces/screenshot-json';
 import * as React from 'react';
+import TimeWithTooltip from 'time-with-tooltip';
+import { UserLink } from 'user-link';
 
 interface Props {
   screenshot: ScreenshotJson;
@@ -13,6 +15,29 @@ export default function Main(props: Props) {
   return (
     <>
       <HeaderV4 />
+
+      <div className='osu-page osu-page--generic-compact'>
+        <div className='screenshot-show'>
+          <div className='screenshot-show__header'>
+            <h1 className='screenshot-show__title'>
+              {props.screenshot.title}
+            </h1>
+            <div className='screenshot-show__detail'>
+              Uploaded by{' '}
+              <UserLink user={props.screenshot.user} />{' '}
+              at{' '}
+              <TimeWithTooltip dateTime={props.screenshot.created_at} relative />
+            </div>
+          </div>
+          <div className='screenshot-show__content'>
+            <img
+              alt={props.screenshot.title}
+              className='screenshot-show__image'
+              src={props.screenshot.image_url ?? ''}
+            />
+          </div>
+        </div>
+      </div>
     </>
   );
 }
