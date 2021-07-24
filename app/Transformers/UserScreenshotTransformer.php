0a6067e1ef80acf6e5ddf01a585e7df7f1f648bb
Gagah Pangeran Rosfatiputra <git@gagahpangeran.com>
2021-07-24 17:27:33 +0700

    add user screenshot transformer

---
+++ app/Transformers/UserScreenshotTransformer.php
@@ -0,0 +1,35 @@
+<?php
+
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
+// See the LICENCE file in the repository root for full licence text.
+
+namespace App\Transformers;
+
+use App\Models\UserScreenshot;
+
+class UserScreenshotTransformer extends TransformerAbstract
+{
+    protected $availableIncludes = [
+        'image_url',
+        'user',
+    ];
+
+    public function transform(UserScreenshot $screenshot)
+    {
+        return [
+            'id' => $screenshot->id,
+            'created_at' => json_time($screenshot->created_at),
+            'title' => $screenshot->title,
+        ];
+    }
+
+    public function includeImageUrl(UserScreenshot $screenshot)
+    {
+        return $this->primitive($screenshot->fileUrl());
+    }
+
+    public function includeUser(UserScreenshot $screenshot)
+    {
+        return $this->item($screenshot->user, new UserCompactTransformer());
+    }
+}
