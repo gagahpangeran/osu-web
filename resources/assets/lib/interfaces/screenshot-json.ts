22a833df09334741499cf5770f53309025a118b5
Gagah Pangeran Rosfatiputra <git@gagahpangeran.com>
2021-07-24 22:51:42 +0700

    add screenshot json interface

---
+++ resources/assets/lib/interfaces/screenshot-json.ts
@@ -0,0 +1,12 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
+// See the LICENCE file in the repository root for full licence text.
+
+import UserJson from './user-json';
+
+export default interface ScreenshotJson {
+  created_at: string;
+  id: number;
+  image_url: string | null;
+  title: string;
+  user: UserJson;
+}
