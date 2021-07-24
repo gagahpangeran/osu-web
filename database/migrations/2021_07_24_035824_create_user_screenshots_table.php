b40a2b59514c3d4959707ec68445557a36cdb01a
Gagah Pangeran Rosfatiputra <git@gagahpangeran.com>
2021-07-24 14:17:45 +0700

    create user screenshots migration

---
+++ database/migrations/2021_07_24_035824_create_user_screenshots_table.php
@@ -0,0 +1,39 @@
+<?php
+
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
+// See the LICENCE file in the repository root for full licence text.
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserScreenshotsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('osu_user_screenshots', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('user_id')->unsigned();
+            $table->string('hash');
+            $table->string('ext');
+            $table->string('title')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('osu_user_screenshots');
+    }
+}
